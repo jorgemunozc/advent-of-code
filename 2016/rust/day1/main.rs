639287448465e62bb0f207ccffff29d17af25106
zh7k0 <6224758+zh7k0@users.noreply.github.com>
2023-06-12 03:22:52 -0400

    fixed degrees in 360 rotation

--- 2016/rust/day1/main.rs
+++ 2016/rust/day1/main.rs
@@ -1,31 +1,22 @@
 use core::panic;
-use std::f32::consts::PI;
+use std::collections::HashMap;
+#[derive(Debug)]
 struct Position {
-    x: f32,
-    y: f32,
-    direction_in_radians: i32,
+    x: i32,
+    y: i32,
+    direction_in_deg: i32,
 }
 
-struct Pos {
-    x: f32,
-    y: f32,
-    facting_to: Facing,
-}
 #[derive(Debug)]
 enum Direction {
     Left,
     Right,
 }
-enum Facing {
-    Nort,
-    South,
-    West,
-    East,
-}
+
 #[derive(Debug)]
 struct Instruction {
     direction: Direction,
-    spaces: f32,
+    spaces: i32,
 }
 
 fn parse_instruction(raw_instruction: &str) -> Instruction {
@@ -42,24 +33,16 @@ fn parse_instruction(raw_instruction: &str) -> Instruction {
 }
 
 fn move_pos(current_pos: &mut Position, instruction: Instruction) {
-    current_pos.direction_in_radians = match instruction.direction {
-        Direction::Left => (current_pos.direction_in_radians + 90) % 360,
-        Direction::Right => (current_pos.direction_in_radians - 90) % 360,
+    let cos = [1, 0, -1, 0];
+    let sin = [0, 1, 0, -1];
+    current_pos.direction_in_deg = match instruction.direction {
+        Direction::Left => ((current_pos.direction_in_deg + 90) % 360 + 360) % 360,
+        Direction::Right => ((current_pos.direction_in_deg - 90) % 360 + 360) % 360,
     };
-    let angle_in_radians = current_pos.direction_in_radians as f32 * (PI / 180.0);
-    current_pos.x += angle_in_radians.cos() * instruction.spaces;
-    current_pos.y += angle_in_radians.cos() * instruction.spaces;
-    println!("x: {} - y: {}", current_pos.x, current_pos.y);
-    println!(
-        "direction: {:?} -  {}",
-        instruction.direction, current_pos.direction_in_radians
-    );
-}
-
-fn move_normal(current_pos: &mut Pos, instruction: Instruction) {
-    match current_pos.facting_to {
-        
-    }
+    let pos = current_pos.direction_in_deg / 90;
+    // println!("deg: {}", current_pos.direction_in_deg);
+    current_pos.x += cos[pos as usize] * instruction.spaces;
+    current_pos.y += sin[pos as usize] * instruction.spaces;
 }
 
 fn distance_from_shortest_route<'a, T>(instructions: T) -> u16
@@ -67,21 +50,32 @@ where
     T: IntoIterator<Item = &'a str>,
 {
     let mut initial_pos = Position {
-        x: 0.0,
-        y: 0.0,
-        direction_in_radians: 90,
+        x: 0,
+        y: 0,
+        direction_in_deg: 90,
     };
+    let mut visited_locations: HashMap<(i32, i32), i32> = HashMap::new();
+    visited_locations.insert((0, 0), 1);
     for instruction in instructions {
         let movement = parse_instruction(instruction);
         move_pos(&mut initial_pos, movement);
-        // println!("We're now in ({}, {})", initial_pos.x, initial_pos.y)
+        println!("");
+        if visited_locations.contains_key(&(initial_pos.x, initial_pos.y)) {
+            println!("posicion que se repite por primera vez {:?}", initial_pos);
+            return (initial_pos.x.abs() + initial_pos.y.abs()) as u16;
+        }
+        visited_locations
+            .entry((initial_pos.x, initial_pos.y))
+            .and_modify(|count| *count += 1)
+            .or_insert(1);
     }
     (initial_pos.x.abs() + initial_pos.y.abs()) as u16
 }
 
 fn main() {
     // let directions = include_str!("../inputs/day1.dat").split(',').map(str::trim);
-    let directions = ["R1", "R1", "R1", "L1", "L1", "R1", "R1", "L1", "R1"];
+    // let directions = ["R1", "R1", "R1", "L1", "L1", "R1", "R1", "L1", "R1"];
+    let directions = ["R8", "R4", "R4", "R8"];
     println!("printin shortest route...");
     let distance_to_destination = distance_from_shortest_route(directions);
     println!("total distance {:?}", distance_to_destination)
@@ -100,5 +94,11 @@ mod tests {
     fn test_goes_to_negative() {
         let directions = ["R1", "R1", "R1", "L1", "L1", "R1", "R1", "L1", "R1"];
         assert_eq!(distance_from_shortest_route(directions), 5);
+    }
+
+    #[test]
+    fn test_distance_first_repeated() {
+        let directions = ["R8", "R4", "R4", "R8"];
+        assert_eq!(distance_from_shortest_route(directions), 4)
     }
 }
